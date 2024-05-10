05ea255372305b1a8c1fa04ed8d2cb8e608dddf4
Keyla Iglesias <kiglesiasesteves@danielcastelao.org>
2024-05-10 09:17:04 +0200

    MOdificación estructura

--- src/cod/mvc/Controller.java
+++ src/cod/mvc/Controller.java
@@ -1,11 +1,10 @@
 package cod.mvc;
 
-import static cod.mvc.Model.CrearCoche;
+import static cod.mvc.Model.crearCoche;
 import static cod.mvc.View.muestraVelocidad;
 
 public class Controller {
     public static void main(String[] args) {
-        CrearCoche("67JUzf", "HUnday", 345);
-        System.out.println("La velocidad del coche es "+ muestraVelocidad("67JUzf", 110));
+        crearCoche("67JUzf", "HUnday", 345);
     }
 }
--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -3,44 +3,64 @@ package cod.mvc;
 import java.util.ArrayList;
 
 public class Model {
-    static ArrayList<Coche> parking = new ArrayList<Coche>();
 
-    public static Coche CrearCoche(String matricula, String modelo, int velocidad){
-        Coche coche = new Coche();
-        coche.matricula = matricula;
-        coche.modelo = modelo;
-        coche.velocidad = velocidad;
+    /**
+     * Lista de coches en el parking
+     */
+    private static ArrayList<Coche> parking = new ArrayList<>();
+
+    /**
+     * Crea un coche y lo añade al parking
+     *
+     * @param matricula Matrícula del coche
+     * @param modelo    Modelo del coche
+     * @param velocidad Velocidad del coche
+     */
+    public static void crearCoche(String matricula, String modelo, int velocidad) {
+        Coche coche = new Coche(matricula, modelo, velocidad);
         parking.add(coche);
-        return coche;
     }
 
+    /**
+     * Busca un coche en el parking por su matrícula
+     *
+     * @param matricula Matrícula del coche a buscar
+     * @return Coche encontrado o null si no existe
+     */
     public static Coche getCoche(String matricula) {
         for (Coche coche : parking) {
-            if (coche.matricula.equals(matricula)) {
-                return coche;
-            }
-            return null;
-        }
-        return null;
-    }
-
-
-    public static Coche CambiarVelocidad(String matricula, int nuevaVelocidad){
-
-        for (Coche coche : parking) {
-            if (coche.matricula.equals(matricula)) {
-                coche.velocidad = nuevaVelocidad;
+            if (coche.getMatricula().equals(matricula)) {
                 return coche;
             }
         }
         return null;
     }
-    public static  int getVelocidad(String matricula){
-        for (Coche coche : parking) {
-            if (coche.matricula.equals(matricula)) {
-                return coche.velocidad;
-            }
+
+    /**
+     * Cambia la velocidad de un coche
+     *
+     * @param matricula Matrícula del coche
+     * @param velocidad Nueva velocidad
+     */
+    public static void cambiarVelocidad(String matricula, int velocidad) {
+        Coche coche = getCoche(matricula);
+        if (coche != null) {
+            coche.setVelocidad(velocidad);
+        }
+    }
+
+    /**
+     * Obtiene la velocidad de un coche
+     *
+     * @param matricula Matrícula del coche
+     * @return Velocidad del coche o -1 si no existe
+     */
+    public static int getVelocidad(String matricula) {
+        Coche coche = getCoche(matricula);
+        if (coche != null) {
+            return coche.getVelocidad();
         }
         return -1;
     }
+
 }
