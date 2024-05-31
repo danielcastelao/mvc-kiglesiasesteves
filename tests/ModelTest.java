c5bc01a72916ed6c0c0471c117a0fcd12d0ef8ee
Keyla Iglesias <kiglesiasesteves@danielcastelao.org>
2024-05-31 08:43:22 +0200

    close #5

--- tests/ModelTest.java
+++ tests/ModelTest.java
@@ -6,18 +6,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class ModelTest {
+    Model model = new Model();
+
     @Test
     public void testCrearCoche() {
-        Model.crearCoche("1234ABC", "Modelo1", 100);
+        model.crearCoche("Modelo1", "1234ABC");
         Coche coche = Model.getCoche("1234ABC");
         assertEquals("1234ABC", coche.getMatricula());
         assertEquals("Modelo1", coche.getModelo());
-        assertEquals(100, coche.getVelocidad());
     }
 
     @Test
     public void testGetCoche() {
-        Model.crearCoche("1234ABC", "Modelo1", 100);
+        model.crearCoche("Modelo1", "1234ABC");
         Coche coche = Model.getCoche("1234ABC");
         assertEquals("1234ABC", coche.getMatricula());
     }
@@ -30,22 +31,9 @@ class ModelTest {
 
     @Test
     public void testCambiarVelocidad() {
-        Model.crearCoche("1234ABC", "Modelo1", 100);
-        Model.cambiarVelocidad("1234ABC", 150);
+        model.crearCoche("Modelo1", "1234ABC");
+        model.cambiarVelocidad("1234ABC", 150);
         Coche coche = Model.getCoche("1234ABC");
         assertEquals(150, coche.getVelocidad());
-    }
-
-    @Test
-    public void testGetVelocidad() {
-        Model.crearCoche("1234ABC", "Modelo1", 100);
-        int velocidad = Model.getVelocidad("1234ABC");
-        assertEquals(100, velocidad);
-    }
-
-    @Test
-    public void testGetVelocidadNotFound() {
-        int velocidad = Model.getVelocidad("0000XYZ");
-        assertEquals(-1, velocidad);
     }
 }
