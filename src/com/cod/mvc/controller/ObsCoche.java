ecccd436c3bf7afa4f47549faa73baeb7f7bfa37
Keyla Iglesias <kiglesiasesteves@danielcastelao.org>
2024-05-31 10:03:14 +0200

    close #7

--- src/com/cod/mvc/controller/ObsCoche.java
+++ src/com/cod/mvc/controller/ObsCoche.java
@@ -7,7 +7,7 @@ import com.cod.mvc.view.View;
 public class ObsCoche implements Observer{
     @Override
     public void update(Coche coche) {
-        System.out.println("[ObserverCoche] Se ha encontrado un coche con esa  matrícula: ");
+         System.out.println("[ObserverCoche] Se ha encontrado un coche con esa  matrícula: ");
         View.muestraTodosDatos(coche);
     }
 }
