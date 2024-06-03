540c9e4e838dab9d00f0cc6ca216c6e1729939b3
keyla <kiglesiasesteves@danielcastelao.org>
2024-06-03 23:07:42 +0200

    Document View

--- src/com/cod/mvc/view/View.java
+++ src/com/cod/mvc/view/View.java
@@ -3,28 +3,49 @@ package com.cod.mvc.view;
 import com.cod.mvc.model.Coche;
 
 /**
- * Clase que muestra la velocidad de un coche
+ * Clase que muestra la información de un coche.
  */
-
 public class View {
     /**
-     * Muestra la velocidad de un coche
+     * Muestra la velocidad de un coche.
      *
-     * @param matricula Matrícula del coche
-     * @param velocidad Velocidad del coche
+     * @param matricula Matrícula del coche.
+     * @param velocidad Velocidad del coche.
      */
     public static void muestraVelocidad(String matricula, int velocidad) {
         System.out.println("La velocidad del coche con " + matricula + " es " + velocidad);
     }
-    public static void muestraTodosDatos(Coche coche){
+
+    /**
+     * Muestra todos los datos de un coche.
+     *
+     * @param coche Objeto Coche cuyos datos se van a mostrar.
+     */
+    public static void muestraTodosDatos(Coche coche) {
         System.out.println("Matrícula: " + coche.getMatricula());
         System.out.println("Modelo: " + coche.getModelo());
         System.out.println("Velocidad: " + coche.getVelocidad());
     }
-    public static void subeVelocidad(String matricula, int velocidad, int previousVelocidad){
-        System.out.println("[ObserverVelocidad] Se ha aumentado la velocidad del coche "+matricula+ " en: " + (velocidad - previousVelocidad)+ "km/hr");
+
+    /**
+     * Informa que la velocidad de un coche ha subido.
+     *
+     * @param matricula Matrícula del coche.
+     * @param velocidad Nueva velocidad del coche.
+     * @param previousVelocidad Velocidad anterior del coche.
+     */
+    public static void subeVelocidad(String matricula, int velocidad, int previousVelocidad) {
+        System.out.println("[ObserverVelocidad] Se ha aumentado la velocidad del coche " + matricula + " en: " + (velocidad - previousVelocidad) + " km/hr");
     }
-    public static void bajaVelocidad(String matricula, int velocidad, int previousVelocidad){
-        System.out.println( "[ObserverVelocidad] Se ha aumentado la velocidad del coche "+matricula+ " en: " + (velocidad- previousVelocidad) + "km/hr");
+
+    /**
+     * Informa que la velocidad de un coche ha bajado.
+     *
+     * @param matricula Matrícula del coche.
+     * @param velocidad Nueva velocidad del coche.
+     * @param previousVelocidad Velocidad anterior del coche.
+     */
+    public static void bajaVelocidad(String matricula, int velocidad, int previousVelocidad) {
+        System.out.println("[ObserverVelocidad] Se ha reducido la velocidad del coche " + matricula + " en: " + (previousVelocidad - velocidad) + " km/hr");
     }
 }
