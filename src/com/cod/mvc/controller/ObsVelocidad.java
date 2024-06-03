a13b6b5aba84913aecbfda59b5e42f1a3e6bd832
keyla <kiglesiasesteves@danielcastelao.org>
2024-06-03 23:04:28 +0200

    Document ObsVelocidad

--- src/com/cod/mvc/controller/ObsVelocidad.java
+++ src/com/cod/mvc/controller/ObsVelocidad.java
@@ -3,27 +3,35 @@ package com.cod.mvc.controller;
 import com.cod.mvc.model.Coche;
 import com.cod.mvc.view.View;
 
-public class ObsVelocidad  implements Observer {
+/**
+ * ObsVelocidad es una implementación de la interfaz Observer.
+ * Se encarga de actualizar la vista cada vez que hay un cambio en la velocidad de un coche.
+ */
+public class ObsVelocidad implements Observer {
 
     /**
-     * Este método es llamado siempre que hay un cambio
-     * El observable cuando hace el notifyObservers
-     * 'dispara' todos los update de los Observers
+     * Este método es llamado siempre que hay un cambio.
+     * El observable, cuando hace el notifyObservers, 'dispara' todos los update de los Observers.
      *
-     * @param arg el argumento pasado por el observable, el coche actualizado
+     * @param arg el argumento pasado por el observable, el coche actualizado.
      */
     @Override
     public void update(Coche arg) {
         if (sube(arg)) {
-View.subeVelocidad(arg.getMatricula(), arg.getVelocidad(), arg.getPreviousVelocidad());        }
-        else {
-
-View.bajaVelocidad(arg.getMatricula(), arg.getVelocidad(), arg.getPreviousVelocidad());        }
+            View.subeVelocidad(arg.getMatricula(), arg.getVelocidad(), arg.getPreviousVelocidad());
+        } else {
+            View.bajaVelocidad(arg.getMatricula(), arg.getVelocidad(), arg.getPreviousVelocidad());
+        }
         // le comunicamos a la vista que muestre la velocidad
         View.muestraVelocidad(arg.getMatricula(), arg.getVelocidad());
-
     }
 
+    /**
+     * Determina si la velocidad del coche ha subido.
+     *
+     * @param arg el coche cuya velocidad se está comparando.
+     * @return true si la velocidad actual es mayor que la velocidad anterior, false en caso contrario.
+     */
     private static boolean sube(Coche arg) {
         return arg.getVelocidad() > arg.getPreviousVelocidad();
     }
